7dc3b70d89e54034162c6f3c770176fc41e503eb
rem <rewmcode@gmail.com>
2024-03-08 13:45:45 +0330

    fix some bugs

--- app/libs/actions.js
+++ app/libs/actions.js
@@ -65,34 +65,4 @@ export const addPackage = async(formData) => {
 }
 
 
-export const updatePackage = async (formData) => {
-    const { id, title, desc, price, stock, color, size } =
-      Object.fromEntries(formData);
-  
-    try {
-      connectToDB();
-  
-      const updateFields = {
-        title,
-        desc,
-        price,
-        stock,
-        color,
-        size,
-      };
-  
-      Object.keys(updateFields).forEach(
-        (key) =>
-          (updateFields[key] === "" || undefined) && delete updateFields[key]
-      );
-  
-      await Product.findByIdAndUpdate(id, updateFields);
-    } catch (err) {
-      console.log(err);
-      throw new Error("Failed to update product!");
-    }
-  
-    revalidatePath("/dashboard/products");
-    redirect("/dashboard/products");
-};
-  
+
